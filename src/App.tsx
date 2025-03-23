e2b82b9f9ce4675047ad62c692a37337a1a5641c
Melone155 <101599412+Melone155@users.noreply.github.com>
2025-03-23 14:40:35 +0100

    If the Webseite doent exist open the error Page

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
 
         <Route path="/shop/root-server" element={<RootServerShop />} />
 
-        <Route path="/notfound" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
     </Router>
